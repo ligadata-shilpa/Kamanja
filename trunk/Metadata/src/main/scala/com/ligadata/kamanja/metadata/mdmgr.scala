183d5bd6de21bf431fcb3f9a520ffa886ee80c75
Rich Johnson <rich@ligadata.com>
2016-01-20 12:53:42 -0800

    Revert "Fatafat218 embellish model listing"
    This reverts commit 050067178c2fae63736e4ee952c1096133868779.

--- trunk/Metadata/src/main/scala/com/ligadata/kamanja/metadata/mdmgr.scala
+++ trunk/Metadata/src/main/scala/com/ligadata/kamanja/metadata/mdmgr.scala
@@ -262,7 +262,6 @@ class MdMgr {
             var newElems = new scala.collection.mutable.ArrayBuffer[T]()
             var newBaseElemsIdxs = scala.collection.mutable.Map[String, Int]()
             es.foreach(e => {
-              /** FIXME: This code cannot pull just the inactive models ... it needs revision */
               if (!e.IsDeleted &&
                 (onlyActive == false || (onlyActive && e.IsActive))) {
                 val fnm = if (e.isInstanceOf[FunctionDef]) e.asInstanceOf[FunctionDef].typeString else e.FullName
