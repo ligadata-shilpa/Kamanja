acc34d3a899909022b015ebcc0d4f9c082032954
jh <jh@ligadata.com>
2016-02-02 22:31:29 -0800

    Fix headers

--- trunk/GenerateModels/jtm/src/test/scala/com/ligadata/jtm/test/FormatMessageTest.scala
+++ trunk/GenerateModels/jtm/src/test/scala/com/ligadata/jtm/test/FormatMessageTest.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 ligaDATA
+ * Copyright 2016 ligaDATA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
