5f59b7fcce7df8fac8d325b23df64454c7698d1c
jh <jh@ligadata.com>
2016-02-05 23:21:06 -0800

    Process computes

--- trunk/GenerateModels/jtm/src/main/scala/com/ligadata/jtm/Compiler.scala
+++ trunk/GenerateModels/jtm/src/main/scala/com/ligadata/jtm/Compiler.scala
@@ -155,7 +155,7 @@ class Compiler(params: CompilerBuilder) extends LogTrait {
     * @return
     */
   def ExtractColumnNames(expression: String): Set[String] = {
-    val regex = """(\$[a-zA-Z0-9_]+)""".r
+    val regex = """(\$[a-zA-Z0-9_.]+)""".r
     regex.findAllMatchIn(expression).toArray.map( m => m.matched.drop(1)).toSet
   }
 
@@ -166,10 +166,10 @@ class Compiler(params: CompilerBuilder) extends LogTrait {
     * @return string with the result
     */
   def FixupColumnNames(expression: String, mapNameSource: Map[String, String]): String = {
-    val regex = """(\$[a-zA-Z0-9_]+)""".r
+    val regex = """(\$[a-zA-Z0-9_.]+)""".r
     val m = regex.pattern.matcher(expression)
     val sb = new StringBuffer
-    var i = 0;
+    var i = 0
     while (m.find) {
       m.appendReplacement(sb, mapNameSource.get(m.group(0).drop(1)).get)
       i = i + 1
@@ -198,6 +198,28 @@ class Compiler(params: CompilerBuilder) extends LogTrait {
       val dups = root.imports.groupBy(identity).collect { case (x,ys) if ys.size > 1 => x }
       logger.warn("Dropped duplicate imports: {}", dups.mkString(", "))
     }
+  }
+
+  def ColumnNames(mgr: MdMgr, classname: String): Set[String] = {
+    val classinstance = md.Message(classname, 0, true)
+    val members = classinstance.get.containerType.asInstanceOf[StructTypeDef].memberDefs
+    members.map( e => e.Name).toSet
+  }
+
+  /**
+    *
+    * @param argName
+    * @param className
+    * @param fieldName
+    */
+  case class Element(argName: String, className: String, fieldName: String)
+
+  def ColumnNames(mgr: MdMgr, classList: Set[String]): Array[Element] = {
+    classList.foldLeft(1, Array.empty[Element])( (r, classname) => {
+      val classMd = md.Message(classname, 0, true)
+      val members = classMd.get.containerType.asInstanceOf[StructTypeDef].memberDefs
+      (r._1 + 1, r._2 ++ members.map( e => Element("msg%d".format(r._1), classname, e.Name)))
+    })._2
   }
 
   // Load metadata
@@ -295,9 +317,9 @@ class Compiler(params: CompilerBuilder) extends LogTrait {
         //
         val name = e._1.head
         val depId = e._2._1
-        val calls = e._2._2.map( f => "exeGenerated_%s_%d(msg)".format(f, depId) ).mkString("\n")
+        val calls = e._2._2.map( f => "exeGenerated_%s_%d(msg1)".format(f, depId) ).mkString("\n")
         result :+= """|if(msg.isInstanceOf[%s]) {
-                      |val msg = msg.isInstanceOf[%s]
+                      |val msg1 = msg.isInstanceOf[%s]
                       |%s
                       |}
                       |""".stripMargin('|').format(name, name, calls)
@@ -312,6 +334,16 @@ class Compiler(params: CompilerBuilder) extends LogTrait {
       throw new Exception("Unsupported multiple dependencies found")
     }
 
+    /*
+    //
+    //
+    def ConstructIsValidMessage(inputTypes: Array[String]): String = {
+      inputTypes.map( e => "    msg.isInstanceOf[%s]".format(e) ).mkString("||\n") + "\n"
+    }
+    subtitutions.Add("factory.isvalidmessage", ConstructIsValidMessage(inputMap.map( p => p._2.handle )))
+    val factory = subtitutions.Run(Parts.factory)
+    */
+
     // Actual function to be called
     //
     dependencyToTransformations.foreach( e => {
@@ -324,8 +356,171 @@ class Compiler(params: CompilerBuilder) extends LogTrait {
         val transformation = root.transformations.get(t).get
 
         result :+= "def exeGenerated_%s_%d(msg: %s) = {".format(t, depId, deps.head)
+
+        // Collect form metadata
+        val inputs: Array[Element] = ColumnNames(md, deps) // Seq("in1", "in2", "in3", "in4").toSet
+
+        // Resolve inputs, either we have unique or qualified names
+        //
+        val uniqueInputs = {
+          val u = inputs.map( e => e.fieldName ).groupBy(identity).mapValues(_.size).filter( f => f._2==1).map( p => p._1)
+          val u1 = u.map( e => inputs.find( c => c.fieldName == e).get)
+          u1.map( p => (p.fieldName -> "%s.%s".format(p.argName, p.fieldName)))
+        }.toMap
+
+        val qualifiedInputs = inputs.map( p => {
+          (p.className + "." + p.fieldName -> "%s.%s".format(p.argName, e))
+        }).toMap
+
+        var mappingSources = uniqueInputs ++ qualifiedInputs
+
+        // Common computes section
+        //
+        var computes = transformation.computes
+        var cnt1 = computes.size
+        var cnt2 = 0
+
+        while(cnt1!=cnt2 && computes.size > 0) {
+          cnt2 = cnt1
+
+          val computes1 = computes.filter(c => {
+
+            val list = ExtractColumnNames(c._2.expression)
+            val rList = list // ResolveNames(list)
+            val open = rList.filter(f => !mappingSources.contains(f) )
+            if(open.size==0) {
+              val newExpression = FixupColumnNames(c._2.expression, mappingSources)
+
+              // Output the actual compute
+              result ++= Array("val %s = %s\n".format(c._1, newExpression))
+              mappingSources ++= Map(c._1 -> c._1)
+              false
+            } else {
+              true
+            }
+          })
+
+          cnt1 = computes1.size
+          computes = computes1
+        }
+
+        if(cnt2!=0){
+          //throw new Exception("Not all elements used")
+          logger.trace("Not all elements used")
+        }
+
+/*
+        // Individual outputs
+        //
+        tr ++= transformation.outputs.foldLeft(Array.empty[String]) ( (r, o) => {
+
+          var collect = Array.empty[String]
+          collect ++= Array("\ndef fn%d() = {\n".format(fncnt))
+
+          val outputSet: Set[String] = ColumnNames(md, o.output) //Seq("out1", "out2", "out3", "out4").toSet
+
+          // State variables to track the progress
+          // a little bit simpler than having val's
+          var mapNameSource: Map[String, String] = inputSet.map( e => (e,"msg.%s".format(e))).toMap
+          var outputSet1: Set[String] = outputSet
+
+          var mapping = o.mappings
+          var filters =  o.filters
+          var computes = o.computes
+          var cnt1 = filters.length + computes.length
+          var cnt2 = 0
+
+          // Removed if mappings are provided
+          val found = mapping.filter( f => mapNameSource.contains(f._2) )
+          found.foreach( f => { outputSet1 --= Set(f._1); mapNameSource ++= Map(f._1 -> mapNameSource.get(f._2).get) } )
+          mapping = mapping.filterKeys( f => !found.contains(f) )
+
+          // Abort this loop if nothing changes or we can satisfy all outputs
+          while(cnt1!=cnt2 && outputSet1.size > 0) {
+
+            cnt2 = cnt1
+
+            // filters
+            val filters1 = filters.filter(f => {
+              val list = ExtractColumnNames(f.expression)
+              val open = list.filter(f => !mapNameSource.contains(f) )
+              if(open.size==0) {
+                // Sub names to
+                val newExpression = FixupColumnNames(f.expression, mapNameSource)
+                // Output the actual filter
+                collect ++= Array("if (%s) return\n".format(newExpression))
+                false
+              } else {
+                true
+              }
+            })
+
+            // computes
+            val computes1 = computes.filter( c => {
+              val list = ExtractColumnNames(c.expression)
+              val open = list.filter(f => !mapNameSource.contains(f) )
+              if(open.size==0) {
+                // Sub names to
+                val newExpression = FixupColumnNames(c.expression, mapNameSource)
+                // Output the actual compute
+                collect ++= Array("val %s = %s\n".format(c.output, newExpression))
+                mapNameSource ++= Map(c.output -> c.output)
+                outputSet1 --= Set(c.output)
+                false
+              } else {
+                true
+              }
+            })
+
+            // Check Mapping
+            if(mapping.size>0)
+            {
+              val found = mapping.filter( f => mapNameSource.contains(f._2) )
+              found.foreach(f => {outputSet1 --= Set(f._1); mapNameSource ++= Map(f._1 -> mapNameSource.get(f._2).get)})
+              mapping = mapping.filterKeys( f => !found.contains(f)  )
+            }
+
+            // Update state
+            cnt1 = filters1.length + computes1.length
+            filters = filters1
+            computes = computes1
+          }
+
+          // Transaction id is in the input
+          // so will just push it back if needed
+          if(inputSet.contains("transactionid")) {
+            outputSet1 --= Set("transactionid")
+          }
+
+          if(outputSet1.size>0){
+            throw new Exception("Not all outputs satisfied. missing=" + outputSet1.mkString(", "))
+            logger.trace("Not all outputs satisfied. missing={}" , outputSet1.mkString(", "))
+          }
+
+          if(cnt2!=0){
+            //throw new Exception("Not all elements used")
+            logger.trace("Not all elements used")
+          }
+
+          // Generate the output for this iteration
+          // Translate outputs to the values
+          val outputElements = outputSet.map( e => {
+            // e.name -> from input, from mapping, from variable
+            "new Result(\"%s\", %s)".format(e, mapNameSource.get(e).get)
+          }).mkString(", ")
+          val outputResult = "result ++= Array[Result](%s)".format(outputElements)
+
+          collect ++= Array(outputResult)
+          collect ++= Array("}\n")
+          collect ++= Array("fn%d()\n".format(fncnt))
+
+          // outputs
+          r ++ collect
+        })
         result :+= "}"
+*/
       })
+
     })
 
     // Create metadata and factory code
@@ -336,8 +531,6 @@ class Compiler(params: CompilerBuilder) extends LogTrait {
 
     // Process inputs
     //
-
-
 
     // Write to output file
     val code = CodeHelper.Indent(result)
@@ -364,13 +557,6 @@ class Compiler(params: CompilerBuilder) extends LogTrait {
     })
     s.toSet.toArray
   }
-
-  //
-  //
-  def ConstructIsValidMessage(inputTypes: Array[String]): String = {
-    inputTypes.map( e => "    msg.isInstanceOf[%s]".format(e) ).mkString("||\n") + "\n"
-  }
-
 
   def FixupColumnNames(expression: String, mapNameSource: Map[String, String]): String = {
     val regex = """(\$[a-zA-Z0-9_]+)""".r
