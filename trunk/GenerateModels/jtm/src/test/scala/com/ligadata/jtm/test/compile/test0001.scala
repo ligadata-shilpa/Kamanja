d7b9ee16a551d1915bf8305b4eb938c6d00a2dde
jh <jh@ligadata.com>
2016-02-09 18:15:40 -0800

    Modified code to support mutiple inputs and outputs

--- trunk/GenerateModels/jtm/src/test/scala/com/ligadata/jtm/test/compile/test0001.scala
+++ trunk/GenerateModels/jtm/src/test/scala/com/ligadata/jtm/test/compile/test0001.scala
@@ -18,6 +18,7 @@ import com.ligadata.KamanjaBase._
 import com.ligadata.KvBase.TimeRange
 import com.ligadata.kamanja.metadata.ModelDef
 import com.ligadata.Utils._
+import scala.reflect.runtime.universe._
 class Factory(modelDef: ModelDef, nodeContext: NodeContext) extends ModelInstanceFactory(modelDef, nodeContext) {
   // if more than 1 input we have to find the correct instance
   override def isValidMessage(msg: MessageContainerBase): Boolean = {
@@ -30,23 +31,41 @@ class Factory(modelDef: ModelDef, nodeContext: NodeContext) extends ModelInstanc
   override def createResultObject(): ModelResultBase = new MappedModelResults()
 }
 class Model(factory: ModelInstanceFactory) extends ModelInstance(factory) {
-  override def execute(txnCtxt: TransactionContext, outputDefault: Boolean): ModelResultBase = {
-    implicit var results: Array[Result] = Array.empty[Result]
-    val msg =  txnCtxt.getMessage()
-    if(msg.isInstanceOf[com.ligadata.kamanja.test001.v1000000.msg1]) {
-      val msg1 = msg.asInstanceOf[com.ligadata.kamanja.test001.v1000000.msg1]
-      exeGenerated_test1_1(msg1)
-    }
-    factory.createResultObject().asInstanceOf[MappedModelResults].withResults(results)
-  }
-  def exeGenerated_test1_1(msg1: com.ligadata.kamanja.test001.v1000000.msg1) (implicit results: Array[Result]) = {
+
+  def exeGenerated_test1_1(msg1: com.ligadata.kamanja.test001.v1000000.msg1): Array[Result] = {
+
     val out3 = msg1.in1 + 1000
-    def process_o1(): Long = {
-      if (!(msg1.in2 != -1 && msg1.in2 < 100)) return 1
+    
+    def process_o1(): Array[Result] = {
+      if (!(msg1.in2 != -1 && msg1.in2 < 100)) return Array.empty[Result]
       val t1: String = "s:" + (msg1.in2).toString()
-      results ++ Array[Result](new Result("transactionId", msg1.transactionId), new Result("out1", msg1.in1), new Result("out4", msg1.in3), new Result("out2", t1), new Result("out3", out3))
-      0
+      Array[Result](new Result("transactionId", msg1.transactionId), new Result("out1", msg1.in1), new Result("out4", msg1.in3), new Result("out2", t1), new Result("out3", out3))
     }
-    process_o1()
+
+    process_o1() // ++ process_*()
+  }
+
+  override def execute(txnCtxt: TransactionContext, outputDefault: Boolean): ModelResultBase = {
+
+    // ToDo we expect an array of messages
+    val msgs =  Array(txnCtxt.getMessage())
+
+    var msg1: com.ligadata.kamanja.test001.v1000000.msg1 = null
+
+    // Match all the messages
+    msgs.foreach( _ match {
+        case msg: com.ligadata.kamanja.test001.v1000000.msg1 => msg1 = msg
+        case _: AnyRef => ;
+    })
+
+    val results: Array[Result] =
+      if(msg1!=null) {
+        exeGenerated_test1_1(msg1)
+      }
+      else {
+        Array.empty[Result]
+      }
+
+    factory.createResultObject().asInstanceOf[MappedModelResults].withResults(results)
   }
 }
