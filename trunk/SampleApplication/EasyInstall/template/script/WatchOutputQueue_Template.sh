8032bd113a705037a5401ec704ccd389485baaa5
ligadata-haitham <haitham@ligadata.com>
2016-02-14 00:24:12 +0200

    fixing commit.

--- trunk/SampleApplication/EasyInstall/template/script/WatchOutputQueue_Template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ "$KAFKA_HOME" = "" ]; then
-    echo "Please set KAFKA_HOME"
-    exit 1
-fi
-$KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic testout_1 --from-beginning
