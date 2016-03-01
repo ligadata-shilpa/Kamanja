b90130036a782197f03b689f7d5498ed93f551dd
Pokuri <pokuri@ligadata.com>
2016-02-29 18:07:00 -0800

    Added single quotes (') around parameter values we pass to Kamanja Install script

--- trunk/Utils/ClusterInstaller/InstallDriver/src/main/scala/com/ligadata/InstallDriver/InstallDriver.scala
+++ trunk/Utils/ClusterInstaller/InstallDriver/src/main/scala/com/ligadata/InstallDriver/InstallDriver.scala
@@ -1440,7 +1440,7 @@ Try again.
 
     val priorInstallDirPath: String = s"$parentPath/$priorInstallDirName"
     val newInstallDirPath: String = s"$parentPath/$newInstallDirName"
-    val installCmd: Seq[String] = Seq("bash", "-c", s"$kamanjaClusterInstallPath --ClusterId $clusterId --WorkingDir $workDir --MetadataAPIConfig $apiConfigPath --NodeConfigPath $nodeConfigPath --TarballPath $tarballPath --ipAddrs $ipDataFile --ipIdTargPaths $ipIdCfgTargDataFile --ipPathPairs $ipPathDataFile --priorInstallDirPath $priorInstallDirPath --newInstallDirPath $newInstallDirPath --installVerificationFile $verifyFilePath --externalJarsDir $externalJarsDir ")
+    val installCmd: Seq[String] = Seq("bash", "-c", s"$kamanjaClusterInstallPath --ClusterId '$clusterId' --WorkingDir '$workDir' --MetadataAPIConfig '$apiConfigPath' --NodeConfigPath '$nodeConfigPath' --TarballPath '$tarballPath' --ipAddrs '$ipDataFile' --ipIdTargPaths '$ipIdCfgTargDataFile' --ipPathPairs '$ipPathDataFile' --priorInstallDirPath '$priorInstallDirPath' --newInstallDirPath '$newInstallDirPath' --installVerificationFile '$verifyFilePath' --externalJarsDir '$externalJarsDir' ")
     val installCmdRep: String = installCmd.mkString(" ")
     printAndLogDebug(s"KamanjaClusterInstall cmd used: \n\n$installCmdRep", log)
 
