6fa07a706809c78c5dc77fca8dba914a5e17d769
jh <jh@ligadata.com>
2016-03-10 21:43:13 -0800

    Add grok code dev sample

--- trunk/GenerateModels/jtm/src/test/scala/grok.scala
+++ trunk/GenerateModels/jtm/src/test/scala/grok.scala
@@ -1,6 +1,3 @@
-    }
-    factory.createResultObject().asInstanceOf[MappedModelResults].withResults(results)
-  }
 /*
 * Copyright 2016 ligaDATA
 *
@@ -16,29 +13,43 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.ligadata.jtm.test.filter
+package com.ligadata.jtm.test.grok
+
+class msg1 {
+  var in1 : Int = 0
+  var in2 : Int = 0
+  var in3 : String = ""
+  var in4 : String = ""
+
+  var rowNumber : Int = 0
+  var transactionId : Int = 0
+  var timePartitionData : Int = 0
+}
+
 import com.ligadata.KamanjaBase._
 import com.ligadata.KvBase.TimeRange
 import com.ligadata.kamanja.metadata.ModelDef
 import com.ligadata.Utils._
+
 class Factory(modelDef: ModelDef, nodeContext: NodeContext) extends ModelInstanceFactory(modelDef, nodeContext) {
   override def isValidMessage(msg: MessageContainerBase): Boolean = {
-    msg.isInstanceOf[com.ligadata.kamanja.test001.v1000000.msg1]
+    msg.isInstanceOf[msg1]
   }
   override def createModelInstance(): ModelInstance = return new Model(this)
-  override def getModelName: String = "com.ligadata.jtm.test.filter"
+  override def getModelName: String = "com.ligadata.jtm.test.grok"
   override def getVersion: String = "0.0.1"
   override def createResultObject(): ModelResultBase = new MappedModelResults()
 }
 class Model(factory: ModelInstanceFactory) extends ModelInstance(factory) {
   override def execute(txnCtxt: TransactionContext, outputDefault: Boolean): ModelResultBase = {
     //
-    def exeGenerated_test1_1(msg1: com.ligadata.kamanja.test001.v1000000.msg1): Array[Result] = {
-      val out3 = msg1.in1 + 1000
+    def exeGenerated_test1_1(msg1: msg1): Array[Result] = {
+      // in scala, type could be optional
+      val out3: Int = msg1.in1 + 1000
       def process_o1(): Array[Result] = {
         if (!(msg1.in2 != -1 && msg1.in2 < 100)) return Array.empty[Result]
         val t1: String = "s:" + (msg1.in2).toString()
-        Array[Result](new Result("transactionId", msg1.transactionId), new Result("out1", msg1.in1), new Result("out4", msg1.in3), new Result("out2", t1), new Result("out3", out3))
+        Array[Result](new Result("rowNumber", msg1.rowNumber), new Result("transactionId", msg1.transactionId), new Result("out1", msg1.in1), new Result("out4", msg1.in3), new Result("out2", t1), new Result("timePartitionData", msg1.timePartitionData), new Result("out3", out3))
       }
       process_o1()
     }
@@ -48,12 +59,15 @@ class Model(factory: ModelInstanceFactory) extends ModelInstance(factory) {
     val msgs = Array(txnCtxt.getMessage()).map(m => m.FullName -> m).toMap
     // Evaluate messages
     //
-    val msg1 = msgs.get("com.ligadata.kamanja.test001.msg1").getOrElse(null).asInstanceOf[com.ligadata.kamanja.test001.v1000000.msg1]
+    val msg1 = msgs.get("msg1").getOrElse(null).asInstanceOf[msg1]
     // Main dependency -> execution check
     //
     val results: Array[Result] =
-    if(msg1!=null) {
-      exeGenerated_test1_1(msg1)
-    } else {
-      Array.empty[Result]
+      if(msg1!=null) {
+        exeGenerated_test1_1(msg1)
+      } else {
+        Array.empty[Result]
+      }
+    factory.createResultObject().asInstanceOf[MappedModelResults].withResults(results)
+  }
 }
