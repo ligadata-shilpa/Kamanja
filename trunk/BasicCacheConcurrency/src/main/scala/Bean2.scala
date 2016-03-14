78ce33e1fa094d7a517232434966674841a924d3
ligadata-saleh <saleh@ligadata.com>
2016-03-14 02:02:37 +0200

    start Node development with infinispan

--- trunk/BasicCacheConcurrency/src/main/scala/Bean2.scala
+++
@@ -1,8 +0,0 @@
-import scala.beans.BeanProperty
-
-/**
-  * Created by Saleh on 3/13/2016.
-  */
-class Bean2(@BeanProperty var a:String,@BeanProperty @transient var b:String) extends  Serializable {
-
-}
