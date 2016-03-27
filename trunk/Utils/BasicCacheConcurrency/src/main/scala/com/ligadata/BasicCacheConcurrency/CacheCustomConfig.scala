a67251e6da976d43a128de06d2f79591f2bae839
ligadata-saleh <saleh@ligadata.com>
2016-03-27 15:31:47 +0200

    adding a new event listener

--- trunk/Utils/BasicCacheConcurrency/src/main/scala/com/ligadata/BasicCacheConcurrency/CacheCustomConfig.scala
+++ trunk/Utils/BasicCacheConcurrency/src/main/scala/com/ligadata/BasicCacheConcurrency/CacheCustomConfig.scala
@@ -2,6 +2,7 @@ package com.ligadata.BasicCacheConcurrency
 
 import java.util.Properties
 
+import net.sf.ehcache.Cache
 import net.sf.ehcache.bootstrap.BootstrapCacheLoader
 import net.sf.ehcache.config.{MemoryUnit, FactoryConfiguration, Configuration, CacheConfiguration}
 import net.sf.ehcache.distribution.jgroups.{JGroupsBootstrapCacheLoaderFactory, JGroupsCacheReplicatorFactory, JGroupsCacheManagerPeerProviderFactory}
@@ -13,6 +14,7 @@ import net.sf.ehcache.event.CacheEventListener
   */
 
 object CacheCustomConfig{
+  val ENABLELISTENER:String = "enableListener"
   val REPLICATE_PUTS:String = "replicatePuts"
   val REPLICATE_UPDATES:String = "replicateUpdates"
   val REPLICATE_UPDATES_VIA_COPY:String = "replicateUpdatesViaCopy"
@@ -44,7 +46,9 @@ class CacheCustomConfig(jsonString:String) extends CacheConfiguration{
   private val json = org.json4s.jackson.JsonMethods.parse(jsonString)
   private val values = json.values.asInstanceOf[Map[String, String]]
 
+
   /*
+             enableListener="false"
              name="Node"
              maxBytesLocalHeap="10000"
              maxBytesLocalDisk="1000"
@@ -92,6 +96,8 @@ class CacheCustomConfig(jsonString:String) extends CacheConfiguration{
   //ADD BOOTSTRAP PROPERTIES
   propertiesBootStrap.setProperty(CacheCustomConfig.BOOTSTRAPASYNCHRONOUSLY,(values.getOrElse(CacheCustomConfig.BOOTSTRAPASYNCHRONOUSLY,"false")).toString)
 
+  private val enableListener = values.getOrElse(CacheCustomConfig.ENABLELISTENER,"false").toBoolean
+
   System.setProperty(CacheCustomConfig.PREFERIPV4STACK,"true")
   System.setProperty(CacheCustomConfig.SKIPUPDATECHECK, "true");
 
@@ -99,8 +105,11 @@ class CacheCustomConfig(jsonString:String) extends CacheConfiguration{
     return config
   }
 
-  def  getListener() : CacheEventListener = {
-    return (new JGroupsCacheReplicatorFactory).createCacheEventListener(properties)
+  def  addListeners(cache:Cache) : Unit = {
+    cache.getCacheEventNotificationService.registerListener((new JGroupsCacheReplicatorFactory).createCacheEventListener(properties))
+    if(enableListener){
+      cache.getCacheEventNotificationService.registerListener(new EventCacheListener)
+    }
   }
 
   def  getBootStrap() : BootstrapCacheLoader = {
