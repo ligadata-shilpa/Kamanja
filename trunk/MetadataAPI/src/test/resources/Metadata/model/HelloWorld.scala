888531d3bcf2017d9f15c7a93b08f2deb406f05a
dhaval kolapkar <dhaval@ligadata.com>
2016-03-29 19:41:57 -0700

    Update model test case

---
+++ trunk/MetadataAPI/src/test/resources/Metadata/model/HelloWorld.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 ligaDATA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ligadata.samples.models
+
+import com.ligadata.KamanjaBase._
+import com.ligadata.kamanja.metadata.ModelDef;
+
+class HelloWorldModelFactory(modelDef: ModelDef, nodeContext: NodeContext) extends ModelInstanceFactory(modelDef, nodeContext) {
+  override def isValidMessage(msg: MessageContainerBase): Boolean = return msg.isInstanceOf[msg1]
+  override def createModelInstance(): ModelInstance = return new HelloWorldModel(this)
+  override def getModelName: String = "HelloWorldModel" 
+  override def getVersion: String = "0.0.2"
+  override def createResultObject(): ModelResultBase = new MappedModelResults()
+}
+
+class HelloWorldModel(factory: ModelInstanceFactory) extends ModelInstance(factory) {
+  
+   override def execute(txnCtxt: TransactionContext, outputDefault: Boolean):ModelResultBase = {
+     
+     var helloWorld : msg1 =  txnCtxt.getMessage().asInstanceOf[msg1]
+     
+        if(helloWorld.score!=1)
+          return null;
+     
+     var actualResults: Array[Result] = Array[Result](new Result("Id",helloWorld.id),
+                                                        new Result("Name",helloWorld.Name),
+                                                        new Result("Score",helloWorld.score))
+     return factory.createResultObject().asInstanceOf[MappedModelResults].withResults(actualResults)
+   }
+   
+  
+}
