b7f5305e8cb2e065e5bbe04ac489d0ee67f598d4
ligadata-ramana <ramana@ligadata.com>
2016-03-30 21:22:30 -0700

    Minor Changes to existing Telecom java/scala models to make them compilable in 1.4.0

--- trunk/SampleApplication/Telecom/metadata/model/SubscriberUsageAlert_Telecom.scala
+++ trunk/SampleApplication/Telecom/metadata/model/SubscriberUsageAlert_Telecom.scala
@@ -34,7 +34,6 @@
 //     
 package com.ligadata.models.samples.models
 
-import com.ligadata.KvBase.{ Key, Value }
 import com.ligadata.KamanjaBase._
 import RddUtils._
 import RddDate._
@@ -263,12 +262,14 @@ class SubscriberUsageAlert(factory: ModelInstanceFactory) extends ModelInstance(
 
     // aggregate account usage
     val actMonthlyUsage = actAggrUsage.thismonthusage + rcntTxn.get.usage
-    actAggrUsage.withthismonthusage(actMonthlyUsage).Save
+    actAggrUsage.set("thismonthusage",actMonthlyUsage);
+    actAggrUsage.save;
 
     // aggregate the usage 
     // aggregate individual subscriber usage
     val subMonthlyUsage = subAggrUsage.thismonthusage + rcntTxn.get.usage
-    subAggrUsage.withthismonthusage(subMonthlyUsage).Save
+    subAggrUsage.set("thismonthusage",subMonthlyUsage);
+    subAggrUsage.save
 
 
     dumpAppLog(logTag + "After Aggregation: Subscriber current month usage => " + subMonthlyUsage + ",Account current month usage => " + actMonthlyUsage)
