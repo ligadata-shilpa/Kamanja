957fb0dc0d4bdefa23457a5c7020785957d4e988
Pokuri <pokuri@ligadata.com>
2016-03-30 20:59:04 -0700

    Concept changes

--- trunk/KamanjaBase/src/main/java/com/ligadata/KamanjaBase/Concept.java
+++ trunk/KamanjaBase/src/main/java/com/ligadata/KamanjaBase/Concept.java
@@ -1,12 +1,17 @@
 package com.ligadata.KamanjaBase;
 
 //FIXME:: Decide whether we need to generate concept class for every concept or use generic class like this
-public class Concept implements ContainerOrConcept {
+public class Concept extends ContainerOrConcept {
 	String key = null;
 	String valueType = null;
 	Object value = null;
 
-	Concept(String key, String valueType, Object value) {
+	public Concept(ConceptFactory factory) {
+		super(factory);
+	}
+
+	public Concept(ConceptFactory factory, String key, String valueType, Object value) {
+		super(factory);
 		this.key = key;
 		this.valueType = valueType;
 		this.value = value;
