c12eea2bf4edca0f4814582a9eae01117719c801
ligadata-haitham <haitham@ligadata.com>
2016-03-31 17:30:01 +0200

    another path fix for Medical app compile config

--- trunk/SampleApplication/EasyInstall/easyInstallKamanja.sh
+++ trunk/SampleApplication/EasyInstall/easyInstallKamanja.sh
@@ -780,8 +780,8 @@ cp * $installPath/Kamanja-$ver210/input/SampleApplications/metadata/type
 cd $srcPath/SampleApplication/Medical/template/script/scala-2.10
 cp -rf * $installPath/Kamanja-$ver210/input/SampleApplications/template/script
 
-#cd $srcPath/SampleApplication/Medical/template/config
-#cp -rf * $installPath/Kamanja-$ver210/config
+cd $srcPath/SampleApplication/Medical/Configs
+cp -rf * $installPath/Kamanja-$ver210/config
 #Medical
 
 #Telecom
@@ -1464,8 +1464,8 @@ cp * $installPath/Kamanja-$ver211/input/SampleApplications/metadata/type
 cd $srcPath/SampleApplication/Medical/template/script/scala-2.11
 cp -rf * $installPath/Kamanja-$ver211/input/SampleApplications/template/script
 
-#cd $srcPath/SampleApplication/Medical/template/config
-#cp -rf * $installPath/Kamanja-$ver211/config
+cd $srcPath/SampleApplication/Medical/Configs
+cp -rf * $installPath/Kamanja-$ver211/config
 #Medical
 
 #Telecom
