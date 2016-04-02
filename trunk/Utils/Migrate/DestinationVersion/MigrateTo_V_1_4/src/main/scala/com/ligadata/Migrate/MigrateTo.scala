fc1934af8df6b9494bc49c2529ec7068ec62208d
ligadata-pokuri <pokuri@ligadata.com>
2016-04-01 17:56:29 -0700

    few more tenantid fixes in Migrate

--- trunk/Utils/Migrate/DestinationVersion/MigrateTo_V_1_4/src/main/scala/com/ligadata/Migrate/MigrateTo.scala
+++ trunk/Utils/Migrate/DestinationVersion/MigrateTo_V_1_4/src/main/scala/com/ligadata/Migrate/MigrateTo.scala
@@ -65,6 +65,7 @@ class MigrateTo_V_1_4 extends MigratableTo {
   private val defaultUserId: Option[String] = Some("kamanja")
   private var _parallelDegree = 0
   private var _mergeContainerAndMessages = true
+  private var _tenantId: String = ""
 
   private val globalExceptions = ArrayBuffer[(String, Throwable)]()
 
@@ -178,7 +179,7 @@ class MigrateTo_V_1_4 extends MigratableTo {
     }
   }
 
-  override def init(destInstallPath: String, apiConfigFile: String, clusterConfigFile: String, sourceVersion: String, unhandledMetadataDumpDir: String, curMigrationSummaryFlPath: String, parallelDegree: Int, mergeContainerAndMessages: Boolean, fromScalaVersion: String, toScalaVersion: String): Unit = {
+  override def init(destInstallPath: String, apiConfigFile: String, clusterConfigFile: String, sourceVersion: String, unhandledMetadataDumpDir: String, curMigrationSummaryFlPath: String, parallelDegree: Int, mergeContainerAndMessages: Boolean, fromScalaVersion: String, toScalaVersion: String, tenantId: String): Unit = {
     isValidPath(apiConfigFile, false, true, "apiConfigFile")
     isValidPath(clusterConfigFile, false, true, "clusterConfigFile")
 
@@ -240,6 +241,7 @@ class MigrateTo_V_1_4 extends MigratableTo {
 
     _parallelDegree = if (parallelDegree <= 1) 1 else parallelDegree
     _mergeContainerAndMessages = mergeContainerAndMessages
+    _tenantId = tenantId
 
     _bInit = true
   }
@@ -594,7 +596,7 @@ class MigrateTo_V_1_4 extends MigratableTo {
                     failed = isFailedStatus(retRes)
 
                     if (failed == false) {
-                      val retRes1 = MetadataAPIImpl.AddModel(MetadataAPI.ModelType.fromString(objFormat), defStr, defaultUserId, Some((defaultUserId.get + "." + cfgnm).toLowerCase), Some(ver))
+                      val retRes1 = MetadataAPIImpl.AddModel(MetadataAPI.ModelType.fromString(objFormat), defStr, defaultUserId, _tenantId, Some((defaultUserId.get + "." + cfgnm).toLowerCase), Some(ver))
                       failed = isFailedStatus(retRes1)
                     }
                   } catch {
@@ -632,7 +634,7 @@ class MigrateTo_V_1_4 extends MigratableTo {
                   var failed = false
 
                   try {
-                    val retRes = MetadataAPIImpl.AddModel(MetadataAPI.ModelType.fromString("kpmml"), mdlDefStr, defaultUserId, Some(dispkey), Some(ver))
+                    val retRes = MetadataAPIImpl.AddModel(MetadataAPI.ModelType.fromString("kpmml"), mdlDefStr, defaultUserId, _tenantId, Some(dispkey), Some(ver))
                     failed = isFailedStatus(retRes)
                   } catch {
                     case e: Exception => {
@@ -661,7 +663,7 @@ class MigrateTo_V_1_4 extends MigratableTo {
                 var failed = false
 
                 try {
-                  val retRes = MetadataAPIImpl.AddMessage(msgDefStr, "JSON", defaultUserId)
+                  val retRes = MetadataAPIImpl.AddMessage(msgDefStr, "JSON", defaultUserId, _tenantId)
                   failed = isFailedStatus(retRes)
                 } catch {
                   case e: Exception => {
@@ -691,7 +693,7 @@ class MigrateTo_V_1_4 extends MigratableTo {
                 var failed = false
 
                 try {
-                  val retRes = MetadataAPIImpl.AddContainer(contDefStr, "JSON", defaultUserId)
+                  val retRes = MetadataAPIImpl.AddContainer(contDefStr, "JSON", defaultUserId, _tenantId)
                   failed = isFailedStatus(retRes)
                 } catch {
                   case e: Exception => {
