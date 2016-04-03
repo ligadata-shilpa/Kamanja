0a3ba9e24477677286ad0f48b77846cb51638be3
ligadata-haitham <haitham@ligadata.com>
2016-04-03 12:05:24 +0200

    fixes to MetadataBootstrap classes

--- trunk/MetadataBootstrap/Bootstrap/src/main/scala_2.11/com/ligadata/kamanja/metadataload/ScalaVersionDependentInit.scala
+++ trunk/MetadataBootstrap/Bootstrap/src/main/scala_2.11/com/ligadata/kamanja/metadataload/ScalaVersionDependentInit.scala
@@ -50,25 +50,27 @@ object ScalaVersionDependentInit {
   */
 
   def initFactoryOfModelInstanceFactories(mgr: MdMgr): Unit = {
-    mgr.AddFactoryOfModelInstanceFactory("com.ligadata.FactoryOfModelInstanceFactory", "JarFactoryOfModelInstanceFactory", ModelRepresentation.JAR, "com.ligadata.FactoryOfModelInstanceFactory.JarFactoryOfModelInstanceFactory$", MetadataLoad.baseTypesVer, null, Array[String]())
-    mgr.AddFactoryOfModelInstanceFactory("com.ligadata.jpmml", "JpmmlFactoryOfModelInstanceFactory", ModelRepresentation.PMML, "com.ligadata.jpmml.JpmmlFactoryOfModelInstanceFactory$", MetadataLoad.baseTypesVer, null, Array[String]())
+    //    mgr.AddFactoryOfModelInstanceFactory("com.ligadata.FactoryOfModelInstanceFactory", "JarFactoryOfModelInstanceFactory", ModelRepresentation.JAR, "com.ligadata.FactoryOfModelInstanceFactory.JarFactoryOfModelInstanceFactory$", MetadataLoad.baseTypesVer, null, Array[String]())
+    //    mgr.AddFactoryOfModelInstanceFactory("com.ligadata.jpmml", "JpmmlFactoryOfModelInstanceFactory", ModelRepresentation.PMML, "com.ligadata.jpmml.JpmmlFactoryOfModelInstanceFactory$", MetadataLoad.baseTypesVer, null, Array[String]())
+    mgr.AddFactoryOfModelInstanceFactory("com.ligadata.FactoryOfModelInstanceFactory", "JarFactoryOfModelInstanceFactory", ModelRepresentation.JAR, "com.ligadata.FactoryOfModelInstanceFactory.JarFactoryOfModelInstanceFactory$", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String]())
+    mgr.AddFactoryOfModelInstanceFactory("com.ligadata.jpmml", "JpmmlFactoryOfModelInstanceFactory", ModelRepresentation.PMML, "com.ligadata.jpmml.JpmmlFactoryOfModelInstanceFactory$", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String]())
   }
 
   def InitTypeDefs(mgr: MdMgr): Unit = {
-    mgr.AddScalar(MdMgr.sysNS, "Any", tAny, "Any", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.AnyImpl")
-    mgr.AddScalar(MdMgr.sysNS, "String", tString, "String", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.StringImpl")
-    mgr.AddScalar(MdMgr.sysNS, "Int", tInt, "Int", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.IntImpl")
-    mgr.AddScalar(MdMgr.sysNS, "Integer", tInt, "Int", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.IntImpl")
-    mgr.AddScalar(MdMgr.sysNS, "Long", tLong, "Long", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.LongImpl")
-    mgr.AddScalar(MdMgr.sysNS, "Boolean", tBoolean, "Boolean", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.BoolImpl")
-    mgr.AddScalar(MdMgr.sysNS, "Bool", tBoolean, "Boolean", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.BoolImpl")
-    mgr.AddScalar(MdMgr.sysNS, "Double", tDouble, "Double", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.DoubleImpl")
-    mgr.AddScalar(MdMgr.sysNS, "Float", tFloat, "Float", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.FloatImpl")
-    mgr.AddScalar(MdMgr.sysNS, "Char", tChar, "Char", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.CharImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Any", tAny, "Any", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.AnyImpl")
+    mgr.AddScalar(MdMgr.sysNS, "String", tString, MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, "String", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.StringImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Int", tInt, "Int", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.IntImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Integer", tInt, "Int", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.IntImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Long", tLong, "Long", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.LongImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Boolean", tBoolean, "Boolean", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.BoolImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Bool", tBoolean, "Boolean", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.BoolImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Double", tDouble, "Double", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.DoubleImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Float", tFloat, "Float", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.FloatImpl")
+    mgr.AddScalar(MdMgr.sysNS, "Char", tChar, "Char", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.CharImpl")
 
-    mgr.AddScalar(MdMgr.sysNS, "date", tLong, "Long", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.LongImpl")
-    mgr.AddScalar(MdMgr.sysNS, "dateTime", tLong, "Long", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.LongImpl")
-    mgr.AddScalar(MdMgr.sysNS, "time", tLong, "Long", MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.LongImpl")
+    mgr.AddScalar(MdMgr.sysNS, "date", tLong, "Long", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.LongImpl")
+    mgr.AddScalar(MdMgr.sysNS, "dateTime", tLong, "Long", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.LongImpl")
+    mgr.AddScalar(MdMgr.sysNS, "time", tLong, "Long", MetadataLoad.baseTypesOwnerId, MetadataLoad.baseTypesTenantId, MetadataLoad.baseTypesUniqId, MetadataLoad.baseTypesElementId, MetadataLoad.baseTypesVer, null, Array[String](), "com.ligadata.BaseTypes.LongImpl")
   }
 
 }
