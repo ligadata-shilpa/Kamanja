9b55c7beae4cefe5ff1f702b93d7bb6d9a0c946a
ligadata-pokuri <pokuri@ligadata.com>
2016-04-22 00:16:18 -0700

    changed .SimpleKafkaProducer package name from nothing to com.ligadata.tools. Here fixed script which is calling it

--- trunk/SampleApplication/HelloWorld/template/script/scala-2.11/PushSampleDataToKafka_Template_HelloWorld.sh
+++ trunk/SampleApplication/HelloWorld/template/script/scala-2.11/PushSampleDataToKafka_Template_HelloWorld.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 KAMANJA_HOME={InstallDirectory}
 
-java -cp $KAMANJA_HOME/lib/system/ExtDependencyLibs2_2.11-1.4.0.jar:$KAMANJA_HOME/lib/system/ExtDependencyLibs_2.11-1.4.0.jar:$KAMANJA_HOME/lib/system/KamanjaInternalDeps_2.11-1.4.0.jar:$KAMANJA_HOME/lib/system/simplekafkaproducer_2.11-1.4.0.jar SimpleKafkaProducer --gz true --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "$KAMANJA_HOME/input/SampleApplications/data/Input_Data_HelloWorld.csv.gz" --partitionkeyidxs "1" --format CSV
+java -cp $KAMANJA_HOME/lib/system/ExtDependencyLibs2_2.11-1.4.0.jar:$KAMANJA_HOME/lib/system/ExtDependencyLibs_2.11-1.4.0.jar:$KAMANJA_HOME/lib/system/KamanjaInternalDeps_2.11-1.4.0.jar:$KAMANJA_HOME/lib/system/simplekafkaproducer_2.11-1.4.0.jar com.ligadata.tools.SimpleKafkaProducer --gz true --topics "testin_1" --threads 1 --topicpartitions 8 --brokerlist "localhost:9092" --files "$KAMANJA_HOME/input/SampleApplications/data/Input_Data_HelloWorld.csv.gz" --partitionkeyidxs "1" --format CSV
