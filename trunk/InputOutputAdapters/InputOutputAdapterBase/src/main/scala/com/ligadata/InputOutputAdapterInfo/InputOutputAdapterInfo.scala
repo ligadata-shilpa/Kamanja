8102071f8f0d3b41e6a0500d6dc4638a7eea8cee
ligadata-pokuri <pokuri@ligadata.com>
2016-04-22 18:23:05 -0700

    taking failed msg

--- trunk/InputOutputAdapters/InputOutputAdapterBase/src/main/scala/com/ligadata/InputOutputAdapterInfo/InputOutputAdapterInfo.scala
+++ trunk/InputOutputAdapters/InputOutputAdapterBase/src/main/scala/com/ligadata/InputOutputAdapterInfo/InputOutputAdapterInfo.scala
@@ -249,11 +249,11 @@ trait ExecContext {
   final def execute(data: Array[Byte], uniqueKey: PartitionUniqueRecordKey, uniqueVal: PartitionUniqueRecordValue, readTmMilliSecs: Long): Unit = {
     val deserializer = ""
     val failedMsg = ""
-    val messageName = ""
+    var messageName = ""
 
     try {
       val (tMsg, tDeserializerName, msgName) = input.deserialize(data)
-      messageName = ""
+      messageName = if (msgName != null) msgName else ""
       LOG.debug("Called Deserialize and got msg:" + (if (tMsg == null) "" else tMsg.getFullTypeName))
       val deserializer = if (tDeserializerName != null) tDeserializerName else ""
       val failedMsg = if (msgName != null) msgName else ""
