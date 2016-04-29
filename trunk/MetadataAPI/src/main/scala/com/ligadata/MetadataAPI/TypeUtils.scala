dabb20943ee9de5584f597bb5b8f65df88c74062
ligadata-pokuri <pokuri@ligadata.com>
2016-04-28 17:06:11 -0700

    All types are not ArrayTypeDef

--- trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/TypeUtils.scala
+++ trunk/MetadataAPI/src/main/scala/com/ligadata/MetadataAPI/TypeUtils.scala
@@ -466,7 +466,7 @@ object TypeUtils {
       logger.debug("Fetch the object " + key + " from database ")
       val obj: (String, Any) = PersistenceUtils.GetObject(key.toLowerCase, "types")
       logger.debug("Deserialize the object " + key)
-      val typ = MetadataAPISerialization.deserializeMetadata(new String(obj._2.asInstanceOf[Array[Byte]])).asInstanceOf[ArrayTypeDef]//serializer.DeserializeObjectFromByteArray(obj._2.asInstanceOf[Array[Byte]])
+      val typ = MetadataAPISerialization.deserializeMetadata(new String(obj._2.asInstanceOf[Array[Byte]])).asInstanceOf[AnyRef]
       if (typ != null) {
         logger.debug("Add the object " + key + " to the cache ")
         MetadataAPIImpl.AddObjectToCache(typ, MdMgr.GetMdMgr)
