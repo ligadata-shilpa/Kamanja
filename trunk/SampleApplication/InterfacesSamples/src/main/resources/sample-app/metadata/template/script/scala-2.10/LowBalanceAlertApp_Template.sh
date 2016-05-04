ac3ef6b8761369fa065520cfad20710e00dde46a
ligadata-pokuri <pokuri@ligadata.com>
2016-05-04 14:15:46 -0700

    Restored Finance application. Yet to test

--- trunk/SampleApplication/InterfacesSamples/src/main/resources/sample-app/metadata/template/script/scala-2.10/LowBalanceAlertApp_Template.sh
+++ trunk/SampleApplication/InterfacesSamples/src/main/resources/sample-app/metadata/template/script/scala-2.10/LowBalanceAlertApp_Template.sh
@@ -14,6 +14,12 @@ $KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add 
 
 $KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add message $KAMANJA_HOME/input/SampleApplications/metadata/message/TransactionMsg_Finance.json TENANTID tenant1
 
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add message $KAMANJA_HOME/input/SampleApplications/metadata/message/LowBalanceAlertOutputMsg.json TENANTID tenant1
+
 $KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties upload compile config $KAMANJA_HOME/config/LBCompileCfg_Finance.json
 
 $KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add model scala $KAMANJA_HOME/input/SampleApplications/metadata/model/LowBalanceAlert_Finance.scala DEPENDSON lowbalancealert TENANTID tenant1
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add adaptermessagebinding FROMFILE $KAMANJA_HOME/config/Finance_Adapter_Binding.json
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add adaptermessagebinding FROMFILE $KAMANJA_HOME/config/SystemMsgs_Adapter_Binding.json
---
+++ trunk/SampleApplication/InterfacesSamples/src/main/resources/sample-app/metadata/template/script/scala-2.11/LowBalanceAlertApp_Template.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+KAMANJA_HOME={InstallDirectory}
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties upload cluster config $KAMANJA_HOME/config/ClusterConfig.json
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustAlertHistory_Finance.json TENANTID tenant1
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustPreferences_Finance.json TENANTID tenant1
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/CustomerInfo_Finance.json TENANTID tenant1
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add container $KAMANJA_HOME/input/SampleApplications/metadata/container/GlobalPreferences_Finance.json TENANTID tenant1
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add message $KAMANJA_HOME/input/SampleApplications/metadata/message/TransactionMsg_Finance.json TENANTID tenant1
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add message $KAMANJA_HOME/input/SampleApplications/metadata/message/LowBalanceAlertOutputMsg.json TENANTID tenant1
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties upload compile config $KAMANJA_HOME/config/LBCompileCfg_Finance.json
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add model scala $KAMANJA_HOME/input/SampleApplications/metadata/model/LowBalanceAlert_Finance.scala DEPENDSON lowbalancealert TENANTID tenant1
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add adaptermessagebinding FROMFILE $KAMANJA_HOME/config/Finance_Adapter_Binding.json
+
+$KAMANJA_HOME/bin/kamanja $KAMANJA_HOME/config/MetadataAPIConfig.properties add adaptermessagebinding FROMFILE $KAMANJA_HOME/config/SystemMsgs_Adapter_Binding.json
