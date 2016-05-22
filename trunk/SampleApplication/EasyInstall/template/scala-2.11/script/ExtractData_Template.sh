6cf3476394bacbf5327ec3a0175d4e8fc2259cdd
ligadata-saleh <saleh@ligadata.com>
2016-05-22 12:58:11 +0300

    adding missing Shebang to some scripts and make sure they are all in top

--- trunk/SampleApplication/EasyInstall/template/scala-2.11/script/ExtractData_Template.sh
+++ trunk/SampleApplication/EasyInstall/template/scala-2.11/script/ExtractData_Template.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 KAMANJA_HOME={InstallDirectory}
 
 java -Dlog4j.configurationFile=file:$KAMANJA_HOME/config/log4j2.xml -cp $KAMANJA_HOME/lib/system/ExtDependencyLibs2_2.11-1.4.1.jar:$KAMANJA_HOME/lib/system/ExtDependencyLibs_2.11-1.4.1.jar:$KAMANJA_HOME/lib/system/KamanjaInternalDeps_2.11-1.4.1.jar:$KAMANJA_HOME/lib/system/extractdata_2.11-1.4.1.jar com.ligadata.ExtractData.ExtractData "$@"
