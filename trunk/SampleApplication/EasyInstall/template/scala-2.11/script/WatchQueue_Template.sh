b21d610c58f1ebc21f29ff89843a6622911a2c24
dhaval kolapkar <dhaval@ligadata.com>
2016-05-24 16:06:25 -0700

    Update the usage trigger and add -from-beginning as optional

--- trunk/SampleApplication/EasyInstall/template/scala-2.11/script/WatchQueue_Template.sh
+++ trunk/SampleApplication/EasyInstall/template/scala-2.11/script/WatchQueue_Template.sh
@@ -3,8 +3,9 @@ if [ "$KAFKA_HOME" = "" ]; then
     echo "Please set KAFKA_HOME"
     exit 1
 fi
-if [ $# -eq 2 ]; then
-    echo "Usage: WatchQueue.sh -topic <TOPIC NAME>  -zookeeper <HOSTNAME:PORT>"
+
+if [ $# -ne 6 ]; then
+    echo "Usage: WatchQueue.sh -topic <TOPIC NAME>  -zookeeper <HOSTNAME:PORT> -from-beginning <true/false>"
     exit 1
 fi
 
@@ -18,7 +19,15 @@ for (( i=1; i<=$#; i++)); do
     i=$((i+1))
     TOPIC="${!i}"
      ;;
+  "-from-beginning")
+    i=$((i+1))
+    if [ "${!i}" == "true" ]; then
+      FROM_BEGIN="--from-beginning"
+    else
+      FROM_BEGIN=""
+    fi 
+     ;;
 esac
 done
 
-$KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper $ZOOKEEPER --topic $TOPIC --from-beginning
+$KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper $ZOOKEEPER --topic $TOPIC $FROM_BEGIN
