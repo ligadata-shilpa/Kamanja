4e774597f2c0a36a9a7add08418f7747a29e84e7
Rich Johnson <rich@ligadata.com>
2016-06-08 01:08:29 -0700

    KAMANJA-435 Python Server bytes it
    Status: compiles.. not tested yet.
    
    Changes:
    1) Messages are accepted as byte stream.  Message integrity checks
    added (md5 start and end marker equivalence check) plus recompute
    of md5 on command-message payload (as done on client side).
    
    2) json formatted error messages issued.
    
    Next:
    
    1) update the commands to return some json instead of raw content
    2) call python directly from client side instead of invoking script. Part
    of change will include passing the location of the kamanja PYTHONPATH
    directory.
    3) test server commands and execute python models from script
    4) integrate client script behavior into python factory of factory project

--- trunk/FactoriesOfModelInstanceFactory/PythonModelPrototype/src/main/python/pythonserver.py
+++ trunk/FactoriesOfModelInstanceFactory/PythonModelPrototype/src/main/python/pythonserver.py
@@ -5,8 +5,10 @@ import socket
 import time
 import argparse
 import sys
-import subprocess
-import imp
+#import subprocess
+#import imp
+import hashlib
+import struct
 
 #sys.path.insert(0, "/home/rich/github1/dev/r1.5.0/kamanja/trunk/FactoriesOfModelInstanceFactory/PythonModelPrototype/src/main/python") 
 #print "sys.modules = " + str(sys.modules)
@@ -82,14 +84,123 @@ for extname in 'addModel', 'removeModel', 'serverStatus', 'executeModel', 'stopS
 	handler = HandlerClass()
 	cmdDict[extname] = handler
 
-# The dispatcher invokes the proper command based upon the first line of the received
-# data.  It dispatches the appropriate handler for it, giving the command access to the
-# model dictionary, host, port, and remaining args from 
-# supplying the remaining arguments/lines from the command as its arg.
-def dispatcher(cmdkey, args):
-	print "processing cmd = " + cmdkey
-	cmd = cmdDict[cmdKey]
-	results = cmd.handler(modelDict, host, port, args)
+
+def unpack(rawmsg):
+	"""
+	This method a) verifies the message ingtegrity by examining
+	the md5 markers and computing the md5 digest on the enclosed
+	json data enclosed in the raw message to certify the message
+	is legit. 
+	From the valid json data, a dictionary is resurrected and the
+	value of key "cmd" is found.  This value in turn is used to 
+	locate the appropriate command handler for processing.
+	If there is no "cmd" value or the md5 checks fail, an error 
+	message is logged and returned.
+	A tuple is returned containing the command key, the reconstituted
+	dictionary resurrected from the json string and an error message field
+	that will be empty ("") if all is well else the error description.	
+	WARNING: supplied message is modified.
+	"""
+	lenMd5Digest = 16
+	lenInt = 4
+	rmsgLen = len(rawmsg)
+	reasonable = rmsgLen > lenMd5Digest * 2 + lenInt
+
+	cmd = ""
+	cmdmap = []
+	errmsg = ""
+	if reasonable == True:
+		startHash = rawmsg[0:lenMd5Digest]
+		del rawmsg[0:lenMd5Digest]
+		payloadLenBytes = rawmsg[0:lenInt]
+		(payloadLen,) = unpack('>I', bytearray(payloadLenBytes)) #big endian
+		del rawmsg[0:lenInt]
+		payloadBytes = rawmsg[0:payloadLen]
+		del rawmsg[0:payloadLen]
+		endHash = rawmsg[0:lenMd5Digest]
+		del rawmsg[0:lenMd5Digest]
+
+		hashesMatch = startHash == endHash
+		if hashesMatch == True:
+			# calculate the md5 for ourselves and compare 
+			h = hashlib.new('md5') # md5 digest used for marker based on msg
+			h.update(payloadBytes)
+			md5hash = h.digest()	
+			validPayload = md5hash == endHash
+			if validPayload == True:
+		 		payload = str(payloadBytes)
+		 		cmdmap = json.loads(payload)
+		 		if "cmd" in cmdParameters and cmdParameters["cmd"] in cmdDict:
+		 			cmd = cmdmap["cmd"]
+		 		else:
+		 			cmdStr = "none supplied"
+		 			if "cmd" in cmdParameters:
+		 				cmdStr = cmdParameters["cmd"]
+		 			errmsg = "There is no command parameter or the command supplied is one that is not known... cmd = {}...legitimate server commands must be one of the following: {}".format(cmdStr, str(cmdDict.keys()))
+			else:
+				errmsg = "while the begin and end digests match, they do not reflect the md5 digest of the supplied message."
+		else:
+			errmsg = "the beginning and ending digests do not match... the input msg is bogus"
+	else:
+		errmsg = "Insufficient bytes supplied to be a reasonable message"
+	return (cmd, cmdmap, errmsg)
+
+def formatWireMsg(msg):
+	"""
+	Format the supplied message for transmission to client.  Format is
+	described in the dispatcher function.
+	"""
+	msgBytes = bytearray(msg)
+	msgBytesLen = len(msgBytes)
+	msgBytesLenArray = struct.pack('>I', msgBytesLen) # big endian
+	h = hashlib.new('md5') # md5 digest used for marker based on msgBytes
+	h.update(msgBytes)
+ 	md5hash = h.digest()	
+ 	msgparts = []
+ 	msgparts.append(md5hash)
+ 	msgparts.append(msgBytesLenArray)
+ 	msgparts.append(msgBytes)
+ 	msgparts.append(md5hash)
+ 	rawmsg = b"".join(msgparts)
+	return rawmsg
+
+def goingDownMsg():
+	"""
+	A stop server command has been received.  Format a standard message and
+	return it
+	"""
+	svrdownMsg = json.dumps({'Server' : host, 'Port' : str(port), 'Message' : "python server stopped by user command"})	
+ 	rawmsg = formatWireMsg(svrdownMsg)
+	return rawmsg
+
+def bogusResultsMsg(errmsg):
+	"""
+	Some junk was sent by client to us.  Issue formatted message with the supplied text
+	"""
+	bogusMsg = json.dumps({'Server' : host, 'Port' : str(port), 'Message' : errmsg})	
+ 	rawmsg = formatWireMsg(bogusMsg)
+	return rawmsg
+
+def dispatcher(rawmsg):
+	"""
+	a. The message byte array passed to and fro has these segments: 
+	<StartMarker>, <DataLen>, <JsonData>, <EndMarker>
+	b.  The markers are currently md5 hashes based upon the <JsonData> string (i.e., sans <DataLen>).
+	c.  The <DataLen> is a 4 byte representation of the length of the
+	<JsonData> string.
+	d.  The <StartMarker> and <EndMarker> should have the same value.  The
+	python server will compute its own md5 hash and compare with the markers.  If they do not match an error message is both logged and returned as the result.
+	e.  The <JsonData> itself is a map.  In addition to the message integrity checks, to be successfully processed, a key must be found in the map == 'cmd' whose value is the command that will interpret the remaining key/value pairs in the dictionary.  Should the 'cmd' key value not refer to a
+	legitimate command,  error is logged and returned.
+	""" 
+	results = ""
+	(cmdKey, cmdParameters, errMsg) = unpack(rawmsg)
+	if len(cmdKey) > 0:
+		print "processing cmd = " + cmdKey + "...argument dict = " + str(cmdParameters)
+		cmd = cmdDict[cmdKey]
+		results = cmd.handler(modelDict, host, port, cmdParameters)
+	else:
+		results = bogusResultsMsg(errMsg)
 	return results
 
 # queue up to 5 requests... could be another value... as written 
@@ -99,24 +210,26 @@ preserveNewLines=True
 serversocket.listen(5)
 result = ''
 while True:                                         
+    # establish a connection
 	conn, addr = serversocket.accept()
 	print('Connected by', addr)
 	while True:
-	    # establish a connection
-	    data = conn.recv(1024)
-	    if not data: break # no more data... 
-	    # split the data on the newline, preserving the new lines, cleaning up those
-	    # first elements with strip function.  The first line is the server command. 
-	    # The rest are processed by the server command that is dispatched.
-	    # 
-	    # Adding magic demarcation, xid... those should all be doable.
-	    dataList = data.splitlines(preserveNewLines)
-	    cmdRaw = dataList.pop(0) # obtain the command from first arg
-	    # ... both cmd and dataList changed by pop
-	    cmd = cmdRaw.strip()
-	    result = dispatcher(cmd,dataList)
+		# get a complete msg 1k at a time... we may want to adjust it up?
+	    msgparts = []
+	    remaining = 1024
+	    while remaining > 0:
+	        data = s.recv(remaining)     # Get available data
+	        msgparts.append(data)        # Add it to list of chunks
+	        if not data: remaining = 0   # no more data...  
+	    rawmsg = b"".join(msgparts)      # Stitch together the complete message
+
+	    result = dispatcher(rawmsg) # string answer ...
 	    if result != 'kill-9': # stop command will return 'kill-9' as value
+	    	wireMsg = formatWireMsg(result)
 	    	conn.sendall(result) 
+	    else: 
+	    	wireMsg =  goingDownMsg
+	    	conn.sendall(wireMsg)
 	    conn.close()
     	break
 	
